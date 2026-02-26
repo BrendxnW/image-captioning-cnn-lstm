7153c53e4178e5fbea919174a82060d185ab648a
BrendxnW <BrendonHorse18@gmail.com>
2026-02-25 21:05:00 -0800

    bug fix, in api, the generate_caption should also intake the image and vocabulary

--- src/scripts/caption_api.py
+++ src/scripts/caption_api.py
@@ -16,24 +16,27 @@ VOCAB_PTH = "vocab_2.pkl"
 _model = None
 _vocab = None
 
+
 def load_once():
     global _model, _vocab
-    if _model is None and _vocab is None:
-        return _model, _vocab
     
+    if _model is not None and _vocab is not None:
+        return _model, _vocab
+
     vocab = load_vocab(VOCAB_PTH)
     pad_idx = vocab.word2idx["<PAD>"]
 
     model = build_model(len(vocab.word2idx), pad_idx).to(DEVICE)
     model.load_state_dict(torch.load(CKPT_PTH, map_location=DEVICE))
     model.eval()
-    
+
     _model, _vocab = model, vocab
     return _model, _vocab
 
 @app.post("/caption")
 async def caption(image: UploadFile = File(...)):
+    model, vocab = load_once()
     data = await image.read()
     img = Image.open(io.BytesIO(data)).convert("RGB")
-    text = generate_caption(img, decode="beam")
+    text = generate_caption(model, img, vocab, decode="beam")
     return {"caption": text}
